20f5a03da83cd470b3ea1f6696ffdcc55b68ea6f
Peter Velkov <kidroca@gmail.com>
2021-08-12 12:18:02 +0300

    Add one more change

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/TodoFilters'
 const FILTER_TITLES = {
   [SHOW_ALL]: 'All',
   [SHOW_ACTIVE]: 'Active',
-  [SHOW_COMPLETED]: 'Completed'
+  [SHOW_COMPLETED]: 'Archived'
 }
 
 class Footer extends Component {
